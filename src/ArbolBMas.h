4515f747b644e500c76189651007ee75702fb58f
ezeperez26 <ezeperez26@gmail.com>
2012-05-27 20:31:47 +0000

    Agrego las clases vacias del arbol B+

---
+++ src/ArbolBMas.h
@@ -0,0 +1,17 @@
+/*
+ * ArbolBMas.h
+ *
+ *  Created on: 27/05/2012
+ *      Author: ezequiel
+ */
+
+#ifndef ARBOLBMAS_H_
+#define ARBOLBMAS_H_
+
+class ArbolBMas {
+public:
+	ArbolBMas();
+	virtual ~ArbolBMas();
+};
+
+#endif /* ARBOLBMAS_H_ */
---
+++ src/BMasNodoHoja.cpp
@@ -0,0 +1,18 @@
+/*
+ * BMasNodoHoja.cpp
+ *
+ *  Created on: 27/05/2012
+ *      Author: ezequiel
+ */
+
+#include "BMasNodoHoja.h"
+
+BMasNodoHoja::BMasNodoHoja() {
+	// TODO Auto-generated constructor stub
+
+}
+
+BMasNodoHoja::~BMasNodoHoja() {
+	// TODO Auto-generated destructor stub
+}
+
