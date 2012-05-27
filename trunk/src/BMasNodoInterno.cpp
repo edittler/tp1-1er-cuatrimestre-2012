bbd7428cf816ed81a9b5dd86f37e00db07ada780
ezeperez26@gmail.com <ezeperez26@gmail.com@fe068b09-06b8-804d-df6d-4aff2b6dd7da>
2012-05-27 20:31:47 +0000

    Agrego las clases vacias del arbol B+

---
+++ trunk/src/BMasNodoInterno.cpp
@@ -0,0 +1,18 @@
+/*
+ * BMasNodoInterno.cpp
+ *
+ *  Created on: 27/05/2012
+ *      Author: ezequiel
+ */
+
+#include "BMasNodoInterno.h"
+
+BMasNodoInterno::BMasNodoInterno() {
+	// TODO Auto-generated constructor stub
+
+}
+
+BMasNodoInterno::~BMasNodoInterno() {
+	// TODO Auto-generated destructor stub
+}
+
