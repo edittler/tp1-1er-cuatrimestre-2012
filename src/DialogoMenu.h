b44258f0f726bb392570e471ce910681aca26ac0
ospin29 <ospin29@gmail.com>
2012-06-02 01:28:09 +0000

    actualizaciones

--- src/DialogoMenu.h
+++ src/DialogoMenu.h
@@ -2,18 +2,29 @@
 #define DIALOGOMENU_H_
 
 #include "Dialogo.h"
+#include "GeneradorDeClaves.h"
+#include "ArbolKD.h"
+#include "Clave.h"
 #include <iostream>
 
 class DialogoMenu: public Dialogo
 {
 public:
-	DialogoMenu();
+	DialogoMenu(ArbolKD *arbol);
 	virtual ~DialogoMenu();
 	void iniciarDialogo();
 
 private:
 	char opcion;
 	void dibujarMenu(char *opcion);
+	void dialogoRangoDeFechas(Fecha *fechaInicial, Fecha *fechaFinal);
+	void dialogoIngresarFechas(Fecha *fechaInicial, Fecha *fechaFinal);
+	void leerFecha(int &dia, int &mes, int &anio);
+	void limpiarFechas();
+	GeneradorDeClaves generadorDeClaves;
+	ArbolKD *arbolKD;
+	Fecha *fechaInicial;
+	Fecha *fechaFinal;
 };
 
 #endif /*DIALOGOMENU_H_*/
