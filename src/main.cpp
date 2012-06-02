a95f064e24007e947737623ccea2fc432d97bbe7
ospin29 <ospin29@gmail.com>
2012-06-02 02:42:12 +0000

    Descomentado

--- src/main.cpp
+++ src/main.cpp
@@ -4,20 +4,57 @@
 #include "ArchivoCSV.h"
 #include "DialogoInstalacion.h"
 #include "DialogoMenu.h"
+#include "ArbolKD.h"
 
 using namespace std;
 
-int main() {
-/*	Pantalla pantalla;
-	ArchivoCSV *archivoCSV = new ArchivoCSV();
+void limpiarLinea(char *linea);
 
-	pantalla.ejecutarDialogo(new DialogoInstalacion(archivoCSV));
-	archivoCSV->cerrarArchivo();
-	pantalla.ejecutarDialogo(new DialogoMenu());*/
+int main() {
+	ArbolKD *arbol = new ArbolKD();
+	Pantalla *pantalla = new Pantalla;
+	ArchivoCSV *archivoCSV = new ArchivoCSV();
+	Parser *parser = new Parser();
+	GeneradorDeClaves *generadorDeClaves = new GeneradorDeClaves();
+	char *lineaDelArchivo;
+	list<string> camposParseados;
+	Clave *claveGenerada;
+
+
+	//-----------------------------------------------------
+	//-----Proceso de instalacion
+	//-----------------------------------------------------
+	pantalla->ejecutarDialogo(new DialogoInstalacion(archivoCSV));
+
+	lineaDelArchivo = new char[100];
+	while(archivoCSV->leerLinea(lineaDelArchivo)) {
+		parser->parsear(lineaDelArchivo, ";", camposParseados);
+		claveGenerada = new Clave();
+		generadorDeClaves->generarClave(camposParseados, claveGenerada);
+		arbol->insertar(claveGenerada);
+		limpiarLinea(lineaDelArchivo);
+	}
+
+
+	//-----------------------------------------------------
+	//-----Proceso de consultas
+	//-----------------------------------------------------
+	pantalla->ejecutarDialogo(new DialogoMenu(arbol));
 
 	return 0;
 
 }
+
+void limpiarLinea(char *linea) {
+	if(linea != NULL)
+		delete []linea;
+	linea = new char[100];
+}
+
+
+
+
+
 
 
 
